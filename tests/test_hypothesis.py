709376053e9b2ef3d33485777e6cb0197a371651
Simon Zeng <contact@simonzeng.com>
2020-11-03 13:28:03 -0500

    Fix flaky test

--- tests/test_hypothesis.py
+++ tests/test_hypothesis.py
@@ -8,16 +8,18 @@ st_naturals = st.integers(min_value=0, max_value=(2 ** 53) - 1)
 st_int = st.integers(min_value=-(2 ** 53) + 1, max_value=(2 ** 53) - 1)
 st_floats = st.floats(min_value=-(2 ** 53) + 1, max_value=(2 ** 53) - 1)
 
+st_text = st.text(alphabet=st.characters(blacklist_categories=["Cn", "Cs"]))
+
 # st.floats would be nice, but then we need pytest.approx, which doesn't work with eg. text
 st_json = st.recursive(
-    st.booleans() | st.text() | st.none() | st_int,  # | st_floats
-    lambda children: st.lists(children) | st.dictionaries(st.text(), children),
+    st.booleans() | st_text | st.none() | st_int,  # | st_floats
+    lambda children: st.lists(children) | st.dictionaries(st_text, children),
 )
 
 keywords = ["as"]
 
 st_passing_json = st.recursive(
-    st.booleans() | st.text() | st_int,  # | st_floats
+    st.booleans() | st_text | st_int,  # | st_floats
     lambda children: st.dictionaries(
         st.text(alphabet=string.ascii_letters).filter(lambda x: x not in keywords),
         children,
@@ -30,7 +32,7 @@ def test_floats(xs):
     assert dhall.loads(dhall.dumps(xs)) == pytest.approx(xs)  # fails when abs=0.05
 
 
-@given(st.text())
+@given(st_text)
 def test_text(xs):
     assert dhall.loads(dhall.dumps(xs)) == xs
 
@@ -52,7 +54,7 @@ def test_list_floats(lst):
     assert dhall.loads(dhall.dumps(lst)) == pytest.approx(lst)
 
 
-@given(st.lists(st.text(), min_size=1))
+@given(st.lists(st_text, min_size=1))
 def test_list_text(lst):
     assert dhall.loads(dhall.dumps(lst)) == lst
 
